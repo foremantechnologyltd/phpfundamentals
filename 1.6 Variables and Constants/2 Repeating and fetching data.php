9621259804cb4f8f212709fd8fc50bb2bcf9f627
foremantechnologyltd <112636023+foremantechnologyltd@users.noreply.github.com>
2022-09-02 14:41:58 +0100

    Module 1.6 created
    Module 1.6 created with examples.
    Index.html updated to reference.

---
+++ 1.6 Variables and Constants/2 Repeating and fetching data.php
@@ -0,0 +1,8 @@
+<?php
+    //This is a simple program that tells us what year it is.
+    $Name = "Daniel"; // We need a book title.
+    $Message = " the current year is... ";
+    $Year = date("y"); // This gets the current year.
+    echo $Name, $Message, "20", $Year; //We use echo to assemble and display the data, inserting a "20" because it only reports
+    // the last two digits of the year.
+?>
